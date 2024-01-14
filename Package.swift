450f05c6fd51c1afff32328c27f8e5f2d1bbfb16
Fausto R <fausto91@gmail.com>
2024-01-14 22:21:05 +0100

    Package rinominato in AdsHelper

--- Package.swift
+++ Package.swift
@@ -4,12 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "AdSupport",
+    name: "AdsHelper",
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "AdSupport",
-            targets: ["AdSupport"]),
+            name: "AdsHelper",
+            targets: ["AdsHelper"]),
     ],
     dependencies: [
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "10.14.0")
@@ -18,12 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AdSupport",
+            name: "AdsHelper",
             dependencies: [
-                "GoogleMobileAds",
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
             ]),
         .testTarget(
-            name: "AdSupportTests",
-            dependencies: ["AdSupport"]),
+            name: "AdsHelperTests",
+            dependencies: ["AdsHelper"]),
     ]
 )
--- Tests/AdsHelperTests/AdsHelperTests.swift
+++ Tests/AdsHelperTests/AdsHelperTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import AdSupport
+@testable import AdsHelper
 
-final class AdSupportTests: XCTestCase {
+final class AdsHelperTests: XCTestCase {
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
